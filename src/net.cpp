sdaftuar/bitcoin

[refactor] Change tx_relay structure to be unique_ptr

src/net.cpp
@@ -498,8 +498,8 @@ void CNode::copyStats(CNodeStats &stats)
     X(addr);
     X(addrBind);
     {
-        LOCK(tx_relay.cs_filter);
-        stats.fRelayTxes = tx_relay.fRelayTxes;
+        LOCK(tx_relay->cs_filter);
+        stats.fRelayTxes = tx_relay->fRelayTxes;
     }
     X(nLastSend);
     X(nLastRecv);
@@ -526,8 +526,8 @@ void CNode::copyStats(CNodeStats &stats)
     }
     X(fWhitelisted);
     {
-        LOCK(tx_relay.cs_feeFilter);
-        stats.minFeeFilter = tx_relay.minFeeFilter;
+        LOCK(tx_relay->cs_feeFilter);
+        stats.minFeeFilter = tx_relay->minFeeFilter;
     }
 
     // It is common for nodes with good ping times to suddenly become lagged,
@@ -815,11 +815,11 @@ bool CConnman::AttemptToEvictConnection()
                 continue;
             if (node->fDisconnect)
                 continue;
-            LOCK(node->tx_relay.cs_filter);
+            LOCK(node->tx_relay->cs_filter);
             NodeEvictionCandidate candidate = {node->GetId(), node->nTimeConnected, node->nMinPingUsecTime,
                                                node->nLastBlockTime, node->nLastTXTime,
                                                HasAllDesirableServiceFlags(node->nServices),
-                                               node->tx_relay.fRelayTxes, node->tx_relay.pfilter != nullptr, node->addr, node->nKeyedNetGroup,
+                                               node->tx_relay->fRelayTxes, node->tx_relay->pfilter != nullptr, node->addr, node->nKeyedNetGroup,
                                                node->m_prefer_evict};
             vEvictionCandidates.push_back(candidate);
         }
@@ -2621,7 +2621,8 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
     addrName = addrNameIn == "" ? addr.ToStringIPPort() : addrNameIn;
     strSubVer = "";
     hashContinue = uint256();
-    tx_relay.pfilter = MakeUnique<CBloomFilter>();
+    tx_relay = MakeUnique<TxRelay>();
+    tx_relay->pfilter = MakeUnique<CBloomFilter>();
 
     for (const std::string &msg : getAllNetMessageTypes())
         mapRecvBytesPerMsgCmd[msg] = 0;

src/net.h
@@ -732,7 +732,7 @@ public:
         TxRelay() : filterInventoryKnown(50000, 0.000001) { filterInventoryKnown.reset(); }
     };
 
-    TxRelay tx_relay;
+    std::unique_ptr<TxRelay> tx_relay;
     // Used for headers announcements - unfiltered blocks to relay
     std::vector<uint256> vBlockHashesToAnnounce GUARDED_BY(cs_inventory);
 
@@ -847,17 +847,17 @@ public:
     void AddInventoryKnown(const CInv& inv)
     {
         {
-            LOCK(tx_relay.cs_tx_inventory);
-            tx_relay.filterInventoryKnown.insert(inv.hash);
+            LOCK(tx_relay->cs_tx_inventory);
+            tx_relay->filterInventoryKnown.insert(inv.hash);
         }
     }
 
     void PushInventory(const CInv& inv)
     {
         if (inv.type == MSG_TX) {
-            LOCK(tx_relay.cs_tx_inventory);
-            if (!tx_relay.filterInventoryKnown.contains(inv.hash)) {
-                tx_relay.setInventoryTxToSend.insert(inv.hash);
+            LOCK(tx_relay->cs_tx_inventory);
+            if (!tx_relay->filterInventoryKnown.contains(inv.hash)) {
+                tx_relay->setInventoryTxToSend.insert(inv.hash);
             }
         } else if (inv.type == MSG_BLOCK) {
             LOCK(cs_inventory);

webpoz/dash

src/net.cpp
@@ -567,8 +567,8 @@ void CNode::copyStats(CNodeStats &stats, const std::vector<bool> &m_asmap)
     X(addrBind);
     stats.m_mapped_as = addr.GetMappedAS(m_asmap);
     {
-        LOCK(m_tx_relay.cs_filter);
-        stats.fRelayTxes = m_tx_relay.fRelayTxes;
+        LOCK(m_tx_relay->cs_filter);
+        stats.fRelayTxes = m_tx_relay->fRelayTxes;
     }
     X(nLastSend);
     X(nLastRecv);
@@ -943,11 +943,11 @@ bool CConnman::AttemptToEvictConnection()
                 }
             }
 
-            LOCK(node->m_tx_relay.cs_filter);
+            LOCK(node->m_tx_relay->cs_filter);
             NodeEvictionCandidate candidate = {node->GetId(), node->nTimeConnected, node->nMinPingUsecTime,
                                                node->nLastBlockTime, node->nLastTXTime,
                                                HasAllDesirableServiceFlags(node->nServices),
-                                               node->m_tx_relay.fRelayTxes, node->m_tx_relay.pfilter != nullptr, node->nKeyedNetGroup,
+                                               node->m_tx_relay->fRelayTxes, node->m_tx_relay->pfilter != nullptr, node->nKeyedNetGroup,
                                                node->m_prefer_evict};
             vEvictionCandidates.push_back(candidate);
         }
@@ -3455,8 +3455,8 @@ void CConnman::RelayInvFiltered(CInv &inv, const CTransaction& relatedTx, const 
         if (pnode->nVersion < minProtoVersion || !pnode->CanRelay())
             continue;
         {
-            LOCK(pnode->m_tx_relay.cs_filter);
-            if(pnode->m_tx_relay.pfilter && !pnode->m_tx_relay.pfilter->IsRelevantAndUpdate(relatedTx))
+            LOCK(pnode->m_tx_relay->cs_filter);
+            if(pnode->m_tx_relay->pfilter && !pnode->m_tx_relay->pfilter->IsRelevantAndUpdate(relatedTx))
                 continue;
         }
         pnode->PushInventory(inv);
@@ -3470,8 +3470,8 @@ void CConnman::RelayInvFiltered(CInv &inv, const uint256& relatedTxHash, const i
         if (pnode->nVersion < minProtoVersion || !pnode->CanRelay())
             continue;
         {
-            LOCK(pnode->m_tx_relay.cs_filter);
-            if(pnode->m_tx_relay.pfilter && !pnode->m_tx_relay.pfilter->contains(relatedTxHash)) continue;
+            LOCK(pnode->m_tx_relay->cs_filter);
+            if(pnode->m_tx_relay->pfilter && !pnode->m_tx_relay->pfilter->contains(relatedTxHash)) continue;
         }
         pnode->PushInventory(inv);
     }
@@ -3621,6 +3621,7 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
     hSocket = hSocketIn;
     addrName = addrNameIn == "" ? addr.ToStringIPPort() : addrNameIn;
     hashContinue = uint256();
+    m_tx_relay = MakeUnique<TxRelay>();
 
     for (const std::string &msg : getAllNetMessageTypes())
         mapRecvBytesPerMsgCmd[msg] = 0;

src/net.h
@@ -1045,7 +1045,7 @@ public:
         std::chrono::microseconds nNextInvSend{0};
     };
 
-    TxRelay m_tx_relay;
+    std::unique_ptr<TxRelay> m_tx_relay;
 
     // Used for headers announcements - unfiltered blocks to relay
     std::vector<uint256> vBlockHashesToAnnounce GUARDED_BY(cs_inventory);
@@ -1200,18 +1200,18 @@ public:
     void AddInventoryKnown(const uint256& hash)
     {
         {
-            LOCK(m_tx_relay.cs_tx_inventory);
-            m_tx_relay.filterInventoryKnown.insert(hash);
+            LOCK(m_tx_relay->cs_tx_inventory);
+            m_tx_relay->filterInventoryKnown.insert(hash);
         }
     }
 
     void PushInventory(const CInv& inv)
     {
         if (inv.type == MSG_TX || inv.type == MSG_DSTX) {
-            LOCK(m_tx_relay.cs_tx_inventory);
-            if (!m_tx_relay.filterInventoryKnown.contains(inv.hash)) {
+            LOCK(m_tx_relay->cs_tx_inventory);
+            if (!m_tx_relay->filterInventoryKnown.contains(inv.hash)) {
                 LogPrint(BCLog::NET, "%s -- adding new inv: %s peer=%d\n", __func__, inv.ToString(), id);
-                m_tx_relay.setInventoryTxToSend.insert(inv.hash);
+                m_tx_relay->setInventoryTxToSend.insert(inv.hash);
             } else {
                 LogPrint(BCLog::NET, "%s -- skipping known inv: %s peer=%d\n", __func__, inv.ToString(), id);
             }
@@ -1221,8 +1221,8 @@ public:
             vInventoryBlockToSend.push_back(inv.hash);
         } else {
             LOCK(cs_inventory);
-            LOCK(m_tx_relay.cs_tx_inventory);
-            if (!m_tx_relay.filterInventoryKnown.contains(inv.hash)) {
+            LOCK(m_tx_relay->cs_tx_inventory);
+            if (!m_tx_relay->filterInventoryKnown.contains(inv.hash)) {
                 LogPrint(BCLog::NET, "%s -- adding new inv: %s peer=%d\n", __func__, inv.ToString(), id);
                 vInventoryOtherToSend.push_back(inv);
             } else {