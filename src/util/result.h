3fc09600479fa7f70a86c83d22e6fdd2abeec36a
pasta <pasta@dashboost.org>
2022-12-15 10:40:11 -0600

    fix: use std::variant, don't require trivial types

--- src/util/result.h
+++ src/util/result.h
@@ -6,10 +6,10 @@
 #define BITCOIN_UTIL_RESULT_H
 
 #include <memory>
+#include <variant>
 
 template<typename T>
 struct Ok {
-    static_assert(std::is_trivially_constructible<T>(), "T must be trivially constructable");
     constexpr explicit Ok(T val) : val_(std::move(val)) {}
     T val_;
 };
@@ -22,18 +22,17 @@ struct Ok<void> {
 template<typename E>
 struct Err {
     constexpr explicit Err(E val) : val_(std::move(val)) {}
-    static_assert(std::is_trivially_constructible<E>(), "E must be trivially constructable");
     E val_;
 };
 
 template<typename T, typename E>
 class Result {
 public:
-    constexpr Result(Ok<T> val) : ok_(true), val_(val) {}
-    constexpr Result(Err<E> err) : ok_(false), err_(err.val_) {}
+    constexpr Result(Ok<T> val) : val_(std::variant<Ok<T>, E>(val)) {}
+    constexpr Result(Err<E> err) : val_(std::variant<Ok<T>, E>(err.val_)) {}
 
-    [[nodiscard]] constexpr bool is_ok() const { return ok_; }
-    [[nodiscard]] constexpr bool is_err() const { return !ok_; }
+    [[nodiscard]] constexpr bool is_ok() const { return std::holds_alternative<Ok<T>>(val_); }
+    [[nodiscard]] constexpr bool is_err() const { return !is_ok(); }
     constexpr explicit operator bool() const {
         return is_ok();
     }
@@ -41,18 +40,16 @@ public:
         return unwrap();
     }
     [[nodiscard]] constexpr auto unwrap() const -> T {
-        assert(ok_);
-        return val_.val_;
+        assert(is_ok());
+        return std::get<T>(val_).val_;
     }
     [[nodiscard]] constexpr auto unwrap_err() const -> E {
-        assert(!ok_);
-        return err_;
+        assert(is_err());
+        return std::get<E>(val_);
     }
 
 private:
-    bool ok_;
-    Ok<T> val_;
-    E err_;
+    std::variant<Ok<T>, E> val_;
 };
 
 #endif
